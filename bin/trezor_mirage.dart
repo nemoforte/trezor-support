2fb82880c0e60d50080eae9f9eca3cfcc6d5606c
Marcin <84280969+nemoforte@users.noreply.github.com>
2024-04-18 14:41:31 +0200

    Initial commit

---
+++ bin/trezor_mirage.dart
@@ -0,0 +1,5 @@
+import 'package:trezor_mirage/trezor_mirage.dart' as trezor_mirage;
+
+void main(List<String> arguments) {
+  print('Hello world: ${trezor_mirage.calculate()}!');
+}
---
+++ test/trezor_mirage_test.dart
@@ -0,0 +1,8 @@
+import 'package:trezor_mirage/trezor_mirage.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
